e5eaa8c0b3ea6cd222754619ecc3d784542737d9
Taeho Jung <tjung@nd.edu>
2020-03-17 20:14:05 -0400

    Test for ND project

--- HEAAN/run/test.cpp
+++ HEAAN/run/test.cpp
@@ -14,37 +14,87 @@
   * There are Encrypt, EncryptSingle, Add, Mult, iMult, RotateFast, Conjugate Tests
   */
 int main(int argc, char **argv) {
+	long logq = 420; ///< Ciphertext Modulus
+	long logp = 16; ///< Real message will be quantized by multiplying 2^(log p + 10)
+	long logn = 0; ///< log2(The number of slots)
+	long n = (1 << logn);
+	srand(time(NULL));
 
-	long logq = 1200; ///< Ciphertext Modulus
-	long logp = 30; ///< Real message will be quantized by multiplying 2^40
-	long logn = 5; ///< log2(The number of slots)
+	cout << "message precision = " << logp << endl;
+    	cout << "log(poly-degree, i.e., n) = " << logN << endl;
+    	cout << "log(temporary expansion) = " << logQ << endl;
+
+
+    	cout << "log(cipher modulus, i.e., q) = " << logq << endl;
+	cout << "multiplicative depth = " << (logq / logp - 1) << endl;
+	cout << "# of packed messages = " << n << endl;
+
+	SetNumThreads(8);
+	TimeUtils timeutils;
+	Ring ring;
+	SecretKey secretKey(ring);
+	Scheme scheme(secretKey, ring);
+
+
+//	timeutils.start("BootKeyGen (one-time)");
+	scheme.addBootKey(secretKey, logn, logp + 10 + 4);
+//	timeutils.stop("BootKeyGen (one-time)");
+
 
 //----------------------------------------------------------------------------------
-//   STANDARD TESTS
+//   MULT TESTS
 //----------------------------------------------------------------------------------
 
-	if(string(argv[1]) == "Encrypt") TestScheme::testEncrypt(logq, logp, logn);
-	if(string(argv[1]) == "EncryptSingle") TestScheme::testEncryptSingle(logq, logp);
-	if(string(argv[1]) == "Add") TestScheme::testAdd(logq, logp, logn);
-	if(string(argv[1]) == "Mult") TestScheme::testMult(logq, logp, logn);
-	if(string(argv[1]) == "iMult") TestScheme::testiMult(logq, logp, logn);
+//	TestScheme::testMult(logq, logp, logn);
 
-//----------------------------------------------------------------------------------
-//   ROTATE & CONJUGATE
-//----------------------------------------------------------------------------------
 
-	long r = 1; ///< The amout of rotation
-	if(string(argv[1]) == "RotateFast") TestScheme::testRotateFast(logq, logp, logn, r);
-	if(string(argv[1]) == "Conjugate") TestScheme::testConjugate(logq, logp, logn);
+
+
+
+	complex<double>* mvec = EvaluatorUtils::randomComplexArray(n);
+
+
+	Ciphertext* cipher = new Ciphertext[100];
+	for(int i = 0; i < 100; i++){
+		scheme.encrypt(cipher[i], mvec, n, logp, logq);
+	}
+
+	timeutils.start("Homomorphic Multiplication for 100 times");
+//	timeutils.start("Homomorphic Multiplication");
+	for(long i = 0; i < 100; i++){
+		scheme.multAndEqual(cipher[rand()], cipher[rand()]);
+	}
+	timeutils.stop("Homomorphic Multiplication for 100 times");	 
+//	timeutils.stop("Homomorphic Multiplication");	
+
     
 //----------------------------------------------------------------------------------
 //   BOOTSTRAPPING
 //----------------------------------------------------------------------------------
-    
-    logq = logp + 10; //< suppose the input ciphertext of bootstrapping has logq = logp + 10
-    logn = 3; //< larger logn will make bootstrapping tech much slower
-    long logT = 4; //< this means that we use Taylor approximation in [-1/T,1/T] with double angle fomula
-    if(string(argv[1]) == "Bootstrapping") TestScheme::testBootstrap(logq, logp, logn, logT);
+
+
+    	logq = (logq % logp) + logp; //< suppose the input ciphertext of bootstrapping has logq = logp + 10
+//    	logn = 1; //< larger logn will make bootstrapping tech much slower
+    	long logT = 4; //< this means that we use Taylor approximation in [-1/T,1/T] with double angle fomula
+
+	
+//	cout << "cipher's logq before bootstrapping = " << cipher.logq << endl;
+//	timeutils.start("Bootstrapping");
+	timeutils.start("Bootstrapping for 100 times");
+	int depth = (logq / logp - 1);
+	int totalq= logq;
+	for(long i = 0; i < 1; i++){
+		scheme.bootstrapAndEqual(cipher, cipher.logq, logQ, logT);
+		depth += (cipher.logq / logp - 1);
+		totalq += cipher.logq;
+	}
+	timeutils.stop("Bootstrapping for 100 times");
+//	timeutils.stop("Bootstrapping");
+	cout << "avg depth = " << (depth / 100.0) << endl;
+	cout << "avg logq = " << (totalq / 100.0) << endl;
+
+
+//    	TestScheme::testBootstrap(logq, logp, logn, logT);
 
 	return 0;
 }
